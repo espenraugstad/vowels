899504e3fecdafa21bd7aa0ef1f619ea88a09b1a
Espen Raugstad <espen.raugstad@gmail.com>
2023-04-01 01:28:25 +0200

    Added C# example

---
+++ C#/Program.cs
@@ -0,0 +1,20 @@
+﻿// Create with .NET 7
+
+string sentence = "The quick brown fox jumps over the lazy dog";
+const string VOWELS = "AEIOUYaeiouy";
+
+// Initiate counter
+int count = 0;
+
+// Can loop directly through a string
+for (int i = 0; i < sentence.Length; i++)
+{
+    // Find the index in VOWELS of the current character. If it is different from -1, it exists and is a vowel
+    if (VOWELS.IndexOf(sentence[i]) != -1)
+    {
+        count++;
+    }
+}
+
+// Print the result
+System.Console.WriteLine(count);
